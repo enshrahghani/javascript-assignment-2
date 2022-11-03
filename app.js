669a70842f8c5ca8a8aba81c3e462484d7d15003
Shahjahan M.Aslam <shahjahanmuhammadaslam@gmail.com>
2022-11-03 13:52:54 +0500

    javascript-assignment-2

---
+++ app.js
@@ -0,0 +1,24 @@
+alert("here are some examples of js")
+
+
+
+var qualification = prompt("enter your qualification here")
+var name = prompt("enter your name here","Guest")
+console.log("name")
+
+
+var age = prompt("Age");
+
+if (age==29){
+    console.log("her age is 29")
+}else{
+    console.log("her age is not 29")
+}
+
+if (age >=1 && age < 8){
+    console.log("they are not allowed")
+}else if(age >= 8 && age < 12){
+    console.log("they are allowed with id card")
+}else{
+    console.log("they are completey allowed")
+}
